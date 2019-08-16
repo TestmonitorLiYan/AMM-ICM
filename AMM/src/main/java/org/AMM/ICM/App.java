fec4f4d4b8c465e0fc7373402c6bea9387ef71e8
lover <15717981520@163.com>
2019-08-16 10:00:52 +0800

    "新建AMM项目"

---
+++ AMM/src/main/java/org/AMM/ICM/App.java
@@ -0,0 +1,13 @@
+package org.AMM.ICM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
